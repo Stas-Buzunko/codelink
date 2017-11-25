cb80d2e6356ee6d952d9896c616ca77f75fd0d19
Stas-Buzunko <buzunkostas@gmail.com>
2017-11-25 17:37:32 +0400

    refactor logger

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import rootReducer from './reducers'
 import { firebaseConfig } from './config'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { reactReduxFirebase } from 'react-redux-firebase'
-import logger from './customLogger'
+import { createLogger } from 'redux-logger';
+
 import JSURL from 'jsurl'
 
 // Add reactReduxFirebase store enhancer
@@ -26,6 +27,32 @@ const initialState = {
   runFromIndex: null,
   results: []
 }
+
+const saveToFirebase = (getState, action) => {
+  const { type, value = null } = action
+  if (type === '@@reactReduxFirebase/LOGIN' || type === '@@reactReduxFirebase/SET_PROFILE') {
+    return false
+  }
+
+  if (window.store) {
+    const { firebase } = window.store
+    const payload = { type, value }
+    const { user } = getState()
+
+    if (user.uid) {
+      payload.uid = user.uid
+      payload.isAnonymous = user.isAnonymous
+    }
+
+    firebase.database().ref('StateChanges').push(payload)
+  }
+
+  return true
+}
+
+const logger = createLogger({
+  predicate: saveToFirebase
+})
 
 // Create store with reducers and initial state
 const store = createStoreWithFirebase(
