d4b60b8dd32599757365c31955e91423b66edec4
Stas-Buzunko <buzunkostas@gmail.com>
2017-11-26 13:21:55 +0400

    rename to upper-case

---
+++ src/Editor.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import AceEditor from 'react-ace'
+
+const Editor = ({ onRun, index, onChange, value = '', result, runAll }) => (
+  <div className="col-lg-12" style={{marginTop: '20px'}}>
+    <div className="col-lg-6">
+      <div className="row">
+        <div className="editor" style={{width: '100%'}}>
+          <AceEditor
+            mode="python"
+            // theme=""
+            value={value}
+            onChange={onChange}
+            editorProps={{$blockScrolling: true}}
+            minLines={1}
+            maxLines={value.split(/\r\n|\r|\n/).length}
+            showLineNumbers={false}
+            commands={[{   // commands is array of key bindings.
+              name: 'executeCode', //name for the key binding.
+              bindKey: { win: 'Shift-Enter', mac: 'Shift-Enter' }, //key combination used for the command.
+              exec: runAll  //function to execute when keys are pressed.
+            }]}
+          />
+        </div>
+      </div>
+      <div className="row" style={{padding: 0, marginTop: '10px'}}>
+        <div className="col-lg-8" style={{paddingLeft: 0}}>
+          <pre id={`editor-result-${index}`} style={{height: '100%'}}>
+            {result}
+          </pre>
+        </div>
+        <div className="col-lg-4">
+          <button
+            className="btn btn-success"
+            onClick={onRun}
+          >
+            Run All Above
+          </button>
+        </div>
+      </div>
+    </div>            
+  </div>
+)
+
+export default Editor
