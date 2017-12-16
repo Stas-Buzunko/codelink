387bf15c3bc9b0ccb79198a5d4b4fb8ac4773cd6
Stas-Buzunko <buzunkostas@gmail.com>
2017-12-16 13:38:23 +0400

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
+import * as Firebase from 'firebase'
+
+global.fbConfig = {
+  apiKey: 'asdf', // placeholder
+  authDomain: 'asdf', // placeholder
+  databaseURL: 'ws://127.0.1:5000',
+  storageBucket: 'asdf', // placeholder
+  messagingSenderId: 'asdf' // placeholder
+}
+
+global.Firebase = Firebase
+
+// Swallow firebase reinitialize error (useful when using watch)
+try {
+  Firebase.initializeApp(fbConfig)
+} catch (err) {}
+
+global.firebase = Object.defineProperty(Firebase, '_', {
+  value: {
+    watchers: {},
+    authUid: null,
+    config: Object.assign(
+      {},
+      fbConfig,
+      {
+        userProfile: 'users',
+        enableRedirectHandling: false // disabled due to lack of http/https
+      }
+    )
+  },
+  writable: true,
+  enumerable: true,
+  configurable: true
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, div);
 });
